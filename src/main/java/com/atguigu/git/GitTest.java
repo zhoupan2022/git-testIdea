e4932f12f764f15164d77825b00abde747260cb8
zp <zp@qq.com>
2022-06-28 19:21:39 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author zp
+ * @date 2022/6/28 - 19:15
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git idea");
+    }
+}
