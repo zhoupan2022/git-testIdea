3fc1f1150bd18750924d58e4e54b1fff9f537789
zp <zp@qq.com>
2022-06-28 19:26:17 +0800

    secord commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,10 +2,11 @@ package com.atguigu.git;
 
 /**
  * @author zp
- * @date 2022/6/28 - 19:15
+ *
  */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git idea");
+        System.out.println("hello git idea2");
     }
 }
