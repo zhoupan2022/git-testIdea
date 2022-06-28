08b380a786e6cc1bbdf0546341bc30b2724c5552
zp <zp@qq.com>
2022-06-28 19:42:30 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git idea");
         System.out.println("hello git idea2");
+        System.out.println("hello git idea3");
+        System.out.println("hello git idea3");
     }
 }
