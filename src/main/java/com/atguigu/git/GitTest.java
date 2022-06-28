bf7726c188588656dd474c3ae72468ed52b72a5d
zp <zp@qq.com>
2022-06-28 19:55:25 +0800

    hot-fix commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello git idea2");
         System.out.println("hello git idea3");
         System.out.println("hello git idea3");
+        System.out.println("hello git idea4");
     }
 }
